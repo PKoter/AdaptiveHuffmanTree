9141cd6756644b67c925e662d309863bd8cb8d54
Brat Wiekszy <bratwiekszy@gmail.com>
2019-04-12 23:13:17 +0200

    Added node tooltip with node data

--- TreeNode.cs
+++ TreeNode.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using JetBrains.Annotations;
 
 namespace HuffmanCompression
@@ -16,6 +17,8 @@ namespace HuffmanCompression
 		public bool IsLeafNode     => Character != null;
 		public bool IsInternalNode => Left != null && Right != null;
 		public bool IsRootNode     => Parent == null;
+
+		public string ActualCode => GetActualCode();
 
 		public TreeNode([CanBeNull] TreeNode parent, int id)
 		{
@@ -58,6 +61,28 @@ namespace HuffmanCompression
 
 			Left.Id = leftId;
 			Right.Id = rightId;
+		}
+
+		public string GetActualCode()
+		{
+			if (IsLeafNode == false)
+				return null;
+
+			var codeBuilder = new StringBuilder(32);
+			int i = 0;
+			var actual = this;
+			var parent = Parent;
+			while (parent != null)
+			{
+				codeBuilder.Append(parent.Right == actual ? '1' : '0');
+				i++;
+				actual = parent;
+				parent = parent.Parent;
+			}
+			var chars = new char[codeBuilder.Length];
+			codeBuilder.CopyTo(0, chars, 0, chars.Length);
+			Array.Reverse(chars);
+			return new string(chars);
 		}
 	}
 }
