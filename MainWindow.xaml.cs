25064c345892d18ecbd45431d4d628741537f78d
Brat Wiekszy <bratwiekszy@gmail.com>
2019-04-12 12:33:25 +0200

    Fixed tree generation. Nodes are drawn correctly. Drawing edges on grid may be problematic tho.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace HuffmanCompression
 {
@@ -23,6 +11,17 @@ namespace HuffmanCompression
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		protected override void OnInitialized(EventArgs e)
+		{
+			base.OnInitialized(e);
+			var tree = new HuffmanTree(256);
+			tree.AddCharacter('a');
+			tree.AddCharacter('b');
+			tree.AddCharacter('c');
+
+			treeComponent.BuildTreeUI(tree);
 		}
 	}
 }
