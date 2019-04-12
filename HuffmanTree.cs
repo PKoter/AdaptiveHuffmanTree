738b5b82cf6bf142cb5e6e35795caf398f3df210
Brat Wiekszy <bratwiekszy@gmail.com>
2019-04-12 12:31:58 +0200

    Added tree data classes

---
+++ HuffmanTree.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace HuffmanCompression
+{
+	public sealed class HuffmanTree
+	{
+		public TreeNode Root { get; private set; }
+
+		[NotNull] private TreeNode       _nytNode;
+		[NotNull] private List<TreeNode> _allNodes;
+
+		public int NodeCount => _allNodes.Count;
+
+
+		public HuffmanTree(int maxId)
+		{
+			Root = new TreeNode(null, maxId);
+			_nytNode = Root;
+			_allNodes = new List<TreeNode>(maxId) { Root };
+		}
+
+
+		public void AddCharacter(char @char)
+		{
+			var exisingCharNode = _allNodes.FirstOrDefault(n => n.Character == @char);
+			if (exisingCharNode != null)
+			{
+				AddedExisingCharacter(exisingCharNode);
+				return;
+			}
+
+			_nytNode.SplitToHoldNewChar(@char);
+			var leafNode = _nytNode.Right;
+			leafNode.IncreaseWeights();
+
+			_nytNode = _nytNode.Left;
+
+			_allNodes.Add(_nytNode);
+			_allNodes.Add(leafNode);
+
+			EnforceLeafInternalNodesInOrder();
+		}
+
+		private void AddedExisingCharacter([NotNull] TreeNode charNode)
+		{
+			charNode.IncreaseWeights();
+		}
+
+		private void EnforceLeafInternalNodesInOrder()
+		{
+			var internalNodes = _allNodes.Where(n => n.IsInternalNode).OrderBy(n => n.Id).ToList();
+			foreach (var internalNode in internalNodes)
+			{
+				var left = internalNode.Left;
+				var right = internalNode.Right;
+				if (left.Weigth != right.Weigth)
+					continue;
+
+				if (left.IsLeafNode && left.Id > right.Id || right.IsLeafNode && right.Id > left.Id)
+				{
+					internalNode.SwapChildNodes();
+				}
+			}
+
+		}
+
+
+		private void RebalanceTree()
+		{
+			var leafNodesByWeight = GetNodesByWeight(n => n.IsLeafNode);
+			var internalNodesByWeight = GetNodesByWeight(n => n.IsInternalNode && !n.IsRootNode);
+
+			foreach (var internalNodes in internalNodesByWeight)
+			{
+				var leafNodes = leafNodesByWeight.First(g => g.Key == internalNodes.Key);
+				var maxIdInternalNode = internalNodes.First();
+				var maxIdLeafNode = leafNodes.First();
+
+				
+
+			}
+		}
+
+		[NotNull]
+		private List<IGrouping<int, TreeNode>> GetNodesByWeight([NotNull] Func<TreeNode, bool> selectPredicate)
+		{
+			return _allNodes.Where(selectPredicate)
+							.OrderByDescending(n => n.Id)
+							.GroupBy(n => n.Weigth)
+							.ToList();
+		}
+	}
+}
---
+++ TreeNode.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace HuffmanCompression
+{
+	public sealed class TreeNode
+	{
+		public TreeNode Parent    { get; private set; }
+		public TreeNode Left      { get; private set; }
+		public TreeNode Right     { get; private set; }
+		public int      Id        { get; private set; }
+		public int      Weigth    { get; private set; }
+		public char?    Character { get; private set; }
+
+		public bool IsNytNode      => Character == null && Weigth == 0 && Left == Right;
+		public bool IsLeafNode     => Character != null;
+		public bool IsInternalNode => Left != null && Right != null;
+		public bool IsRootNode     => Parent == null;
+
+		public TreeNode([CanBeNull] TreeNode parent, int id)
+		{
+			Parent = parent;
+			Id = id;
+		}
+
+		public void SplitToHoldNewChar(char @char)
+		{
+			var nytNode = new TreeNode(this, Id - 2);
+			var charNode = new TreeNode(this, Id - 1);
+			charNode.Character = @char;
+
+			Left = nytNode;
+			Right = charNode;
+		}
+
+		public void IncreaseWeights()
+		{
+			if(IsLeafNode == false)
+				throw new InvalidOperationException();
+
+			Weigth = 1;
+			var upperNode = Parent;
+			while (upperNode != null)
+			{
+				upperNode.Weigth += 1;
+				upperNode = upperNode.Parent;
+			}
+		}
+
+		public void SwapChildNodes()
+		{
+			var leftId = Left.Id;
+			var rightId = Right.Id;
+
+			var temp = Left;
+			Left = Right;
+			Right = temp;
+
+			Left.Id = leftId;
+			Right.Id = rightId;
+		}
+	}
+}
